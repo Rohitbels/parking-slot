654ba215c5bab16b747da89a82f2785a40840fdf
Rohit Belsare <rohit.belsare@thinkproject.com>
2025-02-13 17:24:11 +0530

    routing

---
+++ src/BookingListing.jsx
@@ -0,0 +1,23 @@
+import { Grid2 } from "@mui/material";
+import BookingCard from "./BookingCard"
+
+
+
+
+
+
+const BookingListing = () => {
+
+
+    return <div className="flex ">
+       
+        <BookingCard />
+        <BookingCard />
+        <BookingCard />
+        <BookingCard />
+    
+    </div>
+}
+
+
+export default BookingListing;
